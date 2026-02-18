2399eaf73eae9c566ceecca600768bd0129b80e2
Carlos Gomez Tanco <carlos@mbp-de-carlos.gdl.mex.ibm.com>
2026-02-18 07:04:02 -0600

    third commit with readme file changed from basic course

---
+++ gitautomplete.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.zsh
+curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
